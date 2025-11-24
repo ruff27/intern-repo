d2879794b01a197df1b567d430d671c8bbd5e51d
Ruffin <110576761+ruff27@users.noreply.github.com>
2025-11-24 13:11:58 +1100

    Modified the python file in a new branch (#75)

---
+++ 3. Learn git/main.py
@@ -0,0 +1,13 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+def main():
+    user = "Focus Bear Intern"
+    print(greet(user))
+    print("2 + 3 =", add(2, 3))
+
+if __name__ == "__main__":
+    main()
