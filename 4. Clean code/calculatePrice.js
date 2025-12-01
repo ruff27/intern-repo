24ad6c26f6a9bea029896845b720092774c726f5
Ruffin <110576761+ruff27@users.noreply.github.com>
2025-12-01 12:58:27 +1100

    Add functions to calculate final price with discounts #64

---
+++ 4. Clean code/calculatePrice.js
@@ -0,0 +1,9 @@
+function finalPriceForStudent(price) {
+  const discount = price * 0.1;
+  return price - discount;
+}
+
+function finalPriceForMember(price) {
+  const discount = price * 0.1;
+  return price - discount;
+}
